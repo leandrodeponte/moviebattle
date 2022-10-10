be531c97051c947d08c453c40325c6b55f3bc60f
Leandro De Ponte <ldeponte@bol.com>
2022-10-10 20:50:35 +0200

    Creating Integration tests

--- src/test/java/com/ada/challenge/moviebattle/integration/GameServicesIntegrationTest.java
+++ src/test/java/com/ada/challenge/moviebattle/integration/GameServicesIntegrationTest.java
@@ -1,11 +1,14 @@
 package com.ada.challenge.moviebattle.integration;
 
 import com.ada.challenge.moviebattle.config.domain.GameStatus;
+import com.ada.challenge.moviebattle.controller.dto.GameDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.jdbc.SqlGroup;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
@@ -13,37 +16,53 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
+import static org.springframework.http.converter.json.Jackson2ObjectMapperBuilder.json;
 import static org.springframework.test.context.jdbc.Sql.ExecutionPhase.BEFORE_TEST_METHOD;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class GameControllerIntegrationTest {
+public class GameServicesIntegrationTest {
 
     @Autowired
     private MockMvc mockMvc;
 
     private static final String RANDOM_GAME_ID = UUID.randomUUID().toString();
     private static final String EXISTING_GAME_ID = "1f90a112-cbb4-4fda-ba7f-969b66f132f2";
+    private static final String EXISTING_PLAYER_ID = "b5a3e5a0-4820-11ed-b878-0242ac120002";
 
     @Test
-    public void givenSearchGameURI_whenDoesNotExist_thenReturnNotFound() throws Exception {
+    @SqlGroup({@Sql(value = "classpath:script/game.sql", executionPhase = BEFORE_TEST_METHOD)})
+    public void givenGetGame_whenExists_thenReturnSuccess() throws Exception {
+        mockMvc.perform(get("/api/game/{gameId}", EXISTING_GAME_ID)
+                        .contentType("application/json"))
+                .andExpect(jsonPath("$.id").value(EXISTING_GAME_ID))
+                .andExpect(jsonPath("$.status").value(GameStatus.STARTED.toString()))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void givenGetGame_whenDoesNotExist_thenReturnNotFound() throws Exception {
         mockMvc.perform(get("/api/game/{gameId}", RANDOM_GAME_ID)
                         .contentType("application/json"))
                 .andExpect(status().isNotFound());
     }
 
     @Test
-    @SqlGroup({@Sql(value = "classpath:script/game.sql", executionPhase = BEFORE_TEST_METHOD)})
-    public void givenSearchGameURI_whenExists_thenReturnSuccess() throws Exception {
-        mockMvc.perform(get("/api/game/{gameId}", EXISTING_GAME_ID)
-                        .contentType("application/json"))
-                .andExpect(jsonPath("$.id").value(EXISTING_GAME_ID))
+    public void givenPostGame_whenRequestIsOk_thenReturnSuccess() throws Exception {
+        ObjectMapper mapper = new ObjectMapper();
+        mockMvc.perform(post("/api/game")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(mapper.writeValueAsString(
+                                GameDTO.builder()
+                                        .createdPlayerId(EXISTING_PLAYER_ID)
+                                        .build())))
                 .andExpect(jsonPath("$.status").value(GameStatus.STARTED.toString()))
-                .andExpect(status().isOk());
+                .andExpect(status().isCreated());
     }
 
 }
